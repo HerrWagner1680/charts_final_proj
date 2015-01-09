4d655239a8f59f76e66b828312c8ca96d864dc7f
Sidney Wagner <herrwagner1680@gmail.com>
2015-01-09 14:04:08 -0500

    uploading info into HTML form

--- goog_chart.js
+++ goog_chart.js
@@ -6,6 +6,7 @@ google.load('visualization', '1.0', {'packages':['corechart']});
   // Set a callback to run when the Google Visualization API is loaded.
 google.setOnLoadCallback(drawChart);
 
+
 $( 'select').change(function() {
   if ($('select[name=dataType_col_a]').val() == "string" && 
 		$('select[name=dataType_col_b]').val() == "number") {
@@ -34,7 +35,7 @@ $( 'select').change(function() {
 		data.addColumn($('select[name=dataType_col_b]').val(), 
 					   $('input[name=col_b_label]').val());
 
-
+		console.log("refresh")
 
 	// IF ELSE statements for data-type selection
 
@@ -98,13 +99,7 @@ $( 'select').change(function() {
 // to show error in HTML
 // http://stackoverflow.com/questions/14702190/to-show-error-message-without-alert-box-in-java-script
 
-	//current data.addRows will only work with col a as STRING and b as VALUE
-	    // data.addRows([
-	    //   [($('input[name=cell_1a]').val()), parseInt(($('input[name=cell_1b]').val()))],
-	    //   [($('input[name=cell_2a]').val()), parseInt(($('input[name=cell_2b]').val()))],
-	    //   [($('input[name=cell_3a]').val()), parseInt(($('input[name=cell_3b]').val()))],
-	    //   [($('input[name=cell_4a]').val()), parseInt(($('input[name=cell_4b]').val()))],
-	    // ]);
+
 
 var options = {
 		'title': ($('input[name=chart_title]').val()),
@@ -264,54 +259,51 @@ document.getElementsByName('export_code')[0].value=( yep + ' adsfadfs' +
 
 }); // END OF REFRESH CLICK FUNCTION
 
+console.log("in goog chart js, showing jUrl" + jUrl)
 
  function drawChart() {
 
     // Create the data table.
     var data = new google.visualization.DataTable();
 
-    data.addColumn('number', 'Topping');
-    data.addColumn('number', 'Slices');
 
-    data.addRows([
-      [ 2001, 300],
-      [ 2140, 190],
-      [ 1970, 150],
-      [ 1950, 120],
-      [ 1840, 250]
-    ]);
+  //   data.addColumn('number', 'Topping');
+  //   data.addColumn('number', 'Slices');
 
-  var options = {'title':'How Much Pizza I Ate Last Night',
-	    hAxis: {title: 'Calories', minValue: 1800, maxValue: 2100},
-	    vAxis: {title: 'Slices', minValue: 100, maxValue: 300},
-	    trendlines: {
-	      0: {
-	        // type: 'linear',
-	        //type: 'exponential',
-	        type: 'polynomial',
-	        degree: 3, // degree is used with polynomial
-	        // visibleInLegend: true,
-	      }
-		}
-		};
+  //   data.addRows([
+  //     [ 2001, 300],
+  //     [ 2140, 190],
+  //     [ 1970, 150],
+  //     [ 1950, 120],
+  //     [ 1840, 250]
+  //   ]);
 
-    var areachart = new google.visualization.ScatterChart(document.getElementById('chart_div0'));
+  // var options = {'title':'How Much Pizza I Ate Last Night',
+	 //    hAxis: {title: 'Calories', minValue: 1800, maxValue: 2100},
+	 //    vAxis: {title: 'Slices', minValue: 100, maxValue: 300},
+	 //    trendlines: {
+	 //      0: {
+	 //        // type: 'linear',
+	 //        //type: 'exponential',
+	 //        type: 'polynomial',
+	 //        degree: 3, // degree is used with polynomial
+	 //        // visibleInLegend: true,
+	 //      }
+		// }
+		// };
+
+    var scatterchart = new google.visualization.ScatterChart(document.getElementById('chart_div0'));
 
     // var barchart = new google.visualization.BarChart(document.getElementById('chart_div1'));
-
     // var stepchart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div2'));
-
     // var columnchart = new google.visualization.ColumnChart(document.getElementById('chart_div3'));
-
     // var linechart = new google.visualization.LineChart(document.getElementById('chart_div4'));
-
     // var histogram = new google.visualization.Histogram(document.getElementById('chart_div5'));
-
     // var piechart = new google.visualization.PieChart(document.getElementById('chart_div6'));
 
 
 
-     areachart.draw(data, options);   // str/val ONLY
+     scatterchart.draw(data, options);   // str/val ONLY
     // barchart.draw(data, options);  // str/val ONLY
     // stepchart.draw(data, options);  // str/val ONLY
     // columnchart.draw(data, options);  // val/val and str/val
@@ -322,14 +314,4 @@ document.getElementsByName('export_code')[0].value=( yep + ' adsfadfs' +
 
     };
 
-
-// function handleQueryResponse(response) {
-//   if (response.isError()) {
-//     alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-//     return;
-//   }
-
-//   var data = response.getDataTable();
-
-// }
 
